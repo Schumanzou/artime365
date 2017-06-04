73e3e6b824b6e8831d46bb4ae1319607e4c5cc7c
hn1987 <hn1987@126.com>
2017-06-04 13:28:48 +0800

    test

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
 	'password' => 'd029b2d704',
 	'database' => 'artime365',
