5c61cd5333a10c8218a01956925c91f262fe7925
chenyi <chenyi9@jd.com>
2017-06-06 18:22:50 +0800

    add

--- application/views/api/detail.php
+++ application/views/api/detail.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 <div class="x-body">
-    <div class="x-nav" style="border:0px;font-size:140%;margin-bottom:10px;">
+    <div class="x-nav" style="border:0px;font-size:140%;margin-bottom:10px;padding:0px;">
         第 <?php echo $row->count_id?> 位
         <div class="x-right">
             <i class="icon-play-circled" id="i_start" onclick="playFn()"></i>
