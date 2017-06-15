566ff2b3ec87c744b7a357e78c378bb440dd1bd8
chenyi <chenyi9@jd.com>
2017-06-15 11:33:16 +0800

    加背景

--- application/views/api/detail.php
+++ application/views/api/detail.php
@@ -21,9 +21,9 @@
     <div>
         <img src="/static/images/bg.jpg" width="100%">
     </div>
-    <div class="x-nav" style="border:0px;font-size:140%;margin-bottom:10px;padding:0px;position: absolute;top:0px;">
-        我是第<?php echo $row->count_id?>位为山区孩子说干读诗的人<br/>
-        快来和我一起为爱读诗
+    <div class="x-nav" style="border:0px;font-size:140%;margin-bottom:10px;padding:0px;position: absolute;top:50px;color: white;">
+        <div>我是第<?php echo $row->count_id?>位为山区孩子读诗的人</div>
+        <div>快来和我一起为爱读诗</div>
         <div class="x-right">
             <i class="icon-play-circled" id="i_start" onclick="playFn()"></i>
             <i class="icon-pause-circled" id="i_stop" onclick="playFn()" style="display: none;"></i>
