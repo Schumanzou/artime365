bf290ae9888b7b31330fbbab29d8194b6fa347a7
hn1987 <hn1987@126.com>
2017-06-18 10:50:39 +0800

    bg

--- application/views/api/detail.php
+++ application/views/api/detail.php
@@ -25,7 +25,7 @@
     <div>
         <div class="f-l" style="width:30%"><img src="/static/images/7_02.png" width="100%"></div>
         <div class="f-l" style="width:70%;position: relative;"><img src="/static/images/7_03.png" width="100%">
-            <div style="font-size:140%;position: absolute;color: #ed3f00;letter-spacing:4px;font-weight: bold;top: -5px;left:40px;"><?php echo $row->count_id?></div>
+            <div style="font-size:140%;position: absolute;color: #ed3f00;letter-spacing:4px;font-weight: bold;bottom: 0;"><?php echo $row->count_id?></div>
         </div>
     </div>
     <div>
