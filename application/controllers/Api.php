9ea83aef8b8b8fd9c1824f6370e01e9d3d3ea7be
chenyi <chenyi9@jd.com>
2017-06-27 18:09:00 +0800

    sogou

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -33,19 +33,21 @@ class Api extends MY_Controller{
         $file_name       = time().rand(100, 999).".png";
         $code_url = "/uploads/small/".date('Ym').'/'.$file_name;
         $rankingMd5 = md5($code_url.$this->md5Key);
+        $html5_url = $this->baseUrl."/api/detail/".$rankingMd5;
         $data = array(
             'count_id' => $ranking,
             'url' => $audioUrl,
             'code_url' => $code_url,
             'count_id_md5' => $rankingMd5,
             'type' => $type,
+            'shorturl' => $this->shorturl($html5_url),
         );
 
         $this->db->insert('tbl_audio', $data);
 
         // 返回二维码
         // <img src="http://qr.topscan.com/api.php?bg=f3f3f3&fg=ff0000&gc=222222&el=l&w=200&m=10&text=http://www.topscan.com"/>
-        $content = file_get_contents("http://qr.topscan.com/api.php?w=200&m=10&text=".$this->baseUrl."/api/detail/".$rankingMd5);
+        $content = file_get_contents("http://qr.topscan.com/api.php?w=200&m=10&text=".$html5_url);
 
         $upload_path      = './uploads/small/'.date('Ym').'/';
         if (!file_exists($upload_path)) {
@@ -59,7 +61,7 @@ class Api extends MY_Controller{
         if ($type == 2){
             $this->json(array('ranking'=>$ranking,
                             'sogou'=>'1',
-                            "url"=>$this->baseUrl."/api/detail/".$rankingMd5,
+                            "url"=>$html5_url,
                             "pv_url"=>$this->baseUrl."/api/pv/".$rankingMd5
             ));
         }
@@ -98,13 +100,15 @@ class Api extends MY_Controller{
     /**
      * 生成短域名
      */
-    function shorturl(){
+    private function shorturl($url){
         // 生成短域名
         $this->load->library('curl');
-        $jsonData = $this->curl->simple_get('http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long=http://www.douban.com/note/249723561/');
+        $jsonData = $this->curl->simple_get('http://api.t.sina.com.cn/short_url/shorten.json?source=3271760578&url_long='.$url);
         $res = json_decode($jsonData, true);
-        print_r($res[0]['url_short']);
-        exit;
+        if (isset($res[0]) && isset($res[0]['url_short'])){
+            return $res[0]['url_short'];
+        }
+        return $url;
     }
 
     /**
